c706a2dc4b2474cf530cff540ffe80fd3ee0198a
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-26 23:24:29 +0300

    Create 7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,57 @@
+exec { 'update_ubuntu':
+  command => 'sudo apt-get update',
+  provider => 'shell',
+}
+
+package { 'nginx':
+  ensure => 'installed',
+  require => Exec['update_ubuntu'],
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/html':
+  ensure => 'directory',
+}
+
+file { '/etc/nginx/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+}
+
+file { '/etc/nginx/html/404.html':
+  ensure  => 'file',
+  content => 'Ceci n\'est pas une page',
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root /etc/nginx/html;
+    index index.html;
+    location /redirect_me {
+        return 301 https://johnombuya.pythonanywhere.com;
+    }
+    error_page 404 /404.html;
+    location = /404.html {
+        root /etc/nginx/html;
+        internal;
+    }
+  }",
+  require => File['/etc/nginx/html'],
+}
+
+service { 'nginx-restart':
+  command => 'service nginx restart',
+  require => File['/etc/nginx/sites-available/default'],
+  subscribe => [
+    File['/etc/nginx/html/index.html'],
+    File['/etc/nginx/html/404.html'],
+  ],
+}
